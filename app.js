d0d0523332049edd80a317a24d259206441c6370
parasolid <klaffro@gmail.com>
2016-10-13 17:55:36 +0300

    comments

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-/*
-const http = require("http");
-
-http.get("http://api.fixer.io/latest?base=USD&symbols=RUB", (res) => {
-    var buffer = Buffer.from([]);
-    res.on('data', (chunk) => {
-        buffer = Buffer.concat([buffer, chunk])
-    });
-    res.on('end',()=>{
-        try{
-            var result = JSON.parse(buffer.toString());
-            var ratio = result['rates']['RUB'];
-            console.log(ratio);
-        }catch(e){
-            console.log(e);
-        }
-    })
-});*/
-
 const gpio = require("@tibbo-tps/gpio");
 const lightSensor = require("@tibbo-tps/tibbit-28").init("S25");
 
+// Set up indicator states matrix
 const digits = {
     1: [0,1,0,0,1,0,0,0],
     2: [0,0,1,1,1,1,0,1],
@@ -31,9 +13,20 @@ const digits = {
     8: [0,1,1,1,1,1,1,1],
     9: [0,1,1,0,1,1,1,1],
     0: [0,1,1,1,1,1,1,0],
-    N: [0,0,0,0,0,0,0,1]
+    N: [0,0,0,0,0,0,0,1] // Dash symbol
 };
 
+// Pad function crops array or adds leading zeros
+Array.prototype.pad = function(count){
+    var list = this.slice(-count);
+    while(list.length < count){
+        list.unshift(0)
+    }
+
+    return list;
+};
+
+// Set up pins
 var dataPin = gpio.init("S17A");
 dataPin.setDirection("output");
 dataPin.setValue(0);
@@ -47,45 +40,50 @@ latchPin.setDirection("output");
 latchPin.setValue(0);
 
 const indicate = function(number){
+
+    // Converts number to array of signals to be send
     const numberToSignals = function(number){
-        var digitsList = number
+        return number
             .toString()
-            .split("");
-
-        while(digitsList.length < 3){
-            digitsList.unshift(0)
-        }
-
-        return digitsList
+            .split("")
+            .pad(3)
             .reverse()
             .map(function(value){
                 return digits[value] === undefined ? digits["N"] : digits[value];
             })
-            .reduce(function(previous, current){
+            .reduce(function(previous, current, index, array){
                 return previous.concat(current)
             });
     };
 
     var signals = numberToSignals(number);
 
+    // set ST_CP to LOW
     latchPin.setValue(0);
 
     signals.forEach(function(value){
+        // set DS to value to be pushed
         dataPin.setValue(value);
+
+        // set SH_CP to HIGH and then to LOW
         clockPin.setValue(1);
         clockPin.setValue(0);
     });
 
+    // then all signals are sent put latch to HIGH
     latchPin.setValue(1);
 };
 
 setInterval(function(){
     try{
+        // read illumination value from the sensor
         var illumination = lightSensor.getData().illumination;
         illumination = illumination > 999 ? 999 : illumination;
     }catch(e){
+        // if reading error, set value to indicated to "---"
         illumination = "NNN";
     }
 
+    // display illumination value on the indicators
     indicate(illumination);
 },1000);
